2d4ca2732e3c8520d1ff3ea1e5dd8cec5ddeaea8
Jay Lai <shihjielai@gmail.com>
2020-12-02 16:47:10 +0800

    1st version

---
+++ Test.java
@@ -0,0 +1,23 @@
+package xxx;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		String s1 = "java";
+		StringBuilder s2 = new StringBuilder("java");
+		
+		if (s1 == s2)
+			System.out.println("1");
+		if (s1.equals(s2))
+			System.out.println("2");
+	}
+
+}
